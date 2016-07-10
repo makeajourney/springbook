1d4b3c61e59bdfb7e87b8461c328112aa66df127
Soyoun Kim <makeajourney@gmail.com>
2016-07-10 16:57:10 +0900

    1.2.2 커넥션 만들기의 추출

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -10,8 +10,8 @@ import springbook.user.domain.User;
 
 public class UserDao {
 	public void add(User user) throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/javaweb", "root", "admin");
+		Connection c = getConnection();
+
 		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values (?,?,?)");
 		ps.setString(1, user.getId());
 		ps.setString(2, user.getName());
@@ -24,8 +24,8 @@ public class UserDao {
 	}
 	
 	public User get(String id) throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/javaweb", "root", "admin");
+		Connection c = getConnection();
+		
 		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
 		
@@ -43,5 +43,12 @@ public class UserDao {
 		c.close();
 		
 		return user;
+	}
+	
+	// connection을 가져오는 중복된 코드 분리.
+	private Connection getConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/javaweb", "root", "admin");
+		return c;
 	}
 }
