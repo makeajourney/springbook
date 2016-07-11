0a8cbd380c6bc4015dbaddc9d1cf1739a8cdce6b
Soyoun Kim <makeajourney@gmail.com>
2016-07-11 23:21:50 +0900

    1.7.4 의존관계 주입의 응용

--- src/springbook/user/dao/CountingDaoFactory.java
+++ src/springbook/user/dao/CountingDaoFactory.java
@@ -3,16 +3,20 @@ package springbook.user.dao;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration	// 애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정정보라는 표
-public class DaoFactory {
-	@Bean	// 오브젝트 생성을 담당하는 IoC용 메소드라는 표시 
+@Configuration
+public class CountingDaoFactory {
+	@Bean
 	public UserDao userDao() {
 		return new UserDao(connectionMaker());
 	}
 	
-	// 분리해서 중복을 제거한 ConnectionMaker 타입 오브젝트 생성 코드
 	@Bean
 	public ConnectionMaker connectionMaker() {
+		return new CountingConnectionMaker(realConnectionMaker());
+	}
+	
+	@Bean
+	public ConnectionMaker realConnectionMaker() {
 		return new SimpleConnectionMaker();
 	}
 }
