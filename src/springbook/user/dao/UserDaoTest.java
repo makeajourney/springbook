9f7ba33a457424d327b64238c49656564027b4fc
Soyoun Kim <makeajourney@gmail.com>
2016-07-13 15:35:21 +0900

    2.4.1 테스트를 위한 애플리케이션 컨텍스트 관리

--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -7,13 +7,20 @@ import java.sql.SQLException;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import springbook.user.domain.User;
 
+@RunWith(SpringJUnit4ClassRunner.class)	// 스프링의 테스트 컨텍스트 프레임워크의 JUnit 확장기능 지정.
+@ContextConfiguration(locations="/applicationContext.xml")	// 테스트 컨텍스트가 자동으로 만들어줄 애플리케이션 컨텍스트의 위치 지정.
 public class UserDaoTest {
+	@Autowired
+	private ApplicationContext context;	// 테스트 오브젝트가 만들어지고 나면 스프링 테스트 컨텍스트에 의해 자동으로 값이 주입된다.
 	private UserDao dao;	// setUp() 메소드에서 만드는 오브젝트를 테스트 메소드에서 사용할 수 있도록 인스턴스 변수로 선언한다.
 	private User user1;
 	private User user2;
@@ -21,7 +28,9 @@ public class UserDaoTest {
 	
 	@Before
 	public void setUp() {
-		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		System.out.println(this.context);
+		System.out.println(this);
+		
 		this.dao = context.getBean("userDao", UserDao.class);
 		
 		user1 = new User("gyumee", "박성철", "springno1");
