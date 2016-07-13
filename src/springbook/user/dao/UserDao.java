201dd9ecd0bef1af49d7c65dcd0d4340e324168f
Soyoun Kim <makeajourney@gmail.com>
2016-07-13 20:15:14 +0900

    3.2.2 분리와 재사용을 위한 디자인 패턴 적용

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -102,32 +102,8 @@ public class UserDao {
 	}
 	
 	public void deleteAll() throws SQLException {
-		Connection c = null;
-		PreparedStatement ps = null;
-		
-		try {
-			c = dataSource.getConnection();
-			ps = c.prepareStatement("delete from users");
-			ps.executeUpdate();
-		} catch(SQLException e) {
-			throw e;
-		} finally {
-			if (ps != null)  {
-				try { 
-					ps.close();
-				} catch (SQLException e) {	
-				}
-			}
-			if (c != null) {
-				try {
-					c.close();
-				} catch (SQLException e) {	
-				}
-			}
-		}
-		
-		ps.close();
-		c.close();
+		StatementStrategy st = new DeleteAllStatement();	// 선정한 전략 클래스의 오브젝트 생성. 
+		jdbcContextWithStatementStrategy(st);	// 컨텍스트 호출. 전략 오브젝트 전달.
 	}
 	
 	public int getCount() throws SQLException {
@@ -164,6 +140,24 @@ public class UserDao {
 				} catch (SQLException e) {
 				}
 			}
+		}
+	}
+	
+	public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+		Connection c = null;
+		PreparedStatement ps = null;
+		
+		try {
+			c = dataSource.getConnection();
+			
+			ps = stmt.makePreparedStatement(c);
+			
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (ps != null) { try { ps.close(); } catch (SQLException e) {} }
+			if (c != null) { try { c.close(); } catch (SQLException e) {} }
 		}
 	}
 }
