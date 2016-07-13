913384d1fed41f082c927a91495b9235da97906e
Soyoun Kim <makeajourney@gmail.com>
2016-07-13 20:38:26 +0900

    3.3.2 전략과 클라이언트의 동거 (익명 내부 클래스)

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -19,19 +19,18 @@ public class UserDao {
 	}
 	
 	public void add(final User user) throws SQLException {
-		class AddStatement implements StatementStrategy {
-			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-				PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values (?,?,?)");
-				ps.setString(1, user.getId());
-				ps.setString(2, user.getName());
-				ps.setString(3, user.getPassword());
-				
-				return ps;
+		jdbcContextWithStatementStrategy(
+			new StatementStrategy() {
+				public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+					PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values (?,?,?)");
+					ps.setString(1, user.getId());
+					ps.setString(2, user.getName());
+					ps.setString(3, user.getPassword());
+					
+					return ps;
+				}
 			}
-		}
-		
-		StatementStrategy st = new AddStatement();
-		jdbcContextWithStatementStrategy(st);
+		);
 	}
 	
 	public User get(String id) throws SQLException {
@@ -85,8 +84,13 @@ public class UserDao {
 	}
 	
 	public void deleteAll() throws SQLException {
-		StatementStrategy st = new DeleteAllStatement();	// 선정한 전략 클래스의 오브젝트 생성. 
-		jdbcContextWithStatementStrategy(st);	// 컨텍스트 호출. 전략 오브젝트 전달.
+		jdbcContextWithStatementStrategy(
+			new StatementStrategy() {
+				public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+					return c.prepareStatement("delete from users");
+				}
+			}
+		);	// 컨텍스트 호출. 전략 오브젝트 전달.
 	}
 	
 	public int getCount() throws SQLException {
