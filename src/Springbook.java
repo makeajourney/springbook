f2041962099b329540cb99dc4a62449d7e5414a1
Soyoun Kim <makeajourney@gmail.com>
2016-07-13 11:31:58 +0900

    2.2.2 테스트의 효율적인 수행과 결과 관리

--- src/Springbook.java
+++ src/Springbook.java
@@ -1,37 +1,10 @@
-import java.sql.SQLException;
-
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
-
-import springbook.user.dao.UserDao;
-import springbook.user.domain.User;
+import org.junit.runner.JUnitCore;
 
 public class Springbook {
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-		
-		UserDao dao = context.getBean("userDao", UserDao.class);
-		
-		User user = new User();
-		user.setId("whiteship");
-		user.setName("백기선");
-		user.setPassword("married");
-		
-		dao.add(user);
-		
-		System.out.println(user.getId() + " 등록 성공");
-		
-		User user2 = dao.get(user.getId());
-		if (!user.getName().equals(user2.getName())) {
-			System.out.println("테스트 실패 (name)");
-		} else if (!user.getPassword().equals(user2.getPassword())) {
-			System.out.println("테스트 실패 (password)");
-		} else {
-			System.out.println("조회 테스트 성공");
-		}
+		JUnitCore.main("springbook.user.dao.UserDaoTest");
 	}
 
 }
---
+++ src/springbook/user/dao/UserDaoTest.java
@@ -0,0 +1,35 @@
+package springbook.user.dao;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.hamcrest.CoreMatchers.is;	// is()
+import static org.junit.Assert.assertThat;	// assertThat() 
+
+import springbook.user.domain.User;
+
+public class UserDaoTest {
+	
+	@Test	// JUnit에게 테스트용 메소드임을 알려준다.
+	// JUnit 테스트 메소드는 반드시 public으로 선언돼야 한다.
+	public void addAndGet() throws SQLException {
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		
+		User user = new User();
+		user.setId("gyumee");
+		user.setName("박성철");
+		user.setPassword("springno1");
+		
+		dao.add(user);
+		
+		User user2 = dao.get(user.getId());
+		
+		assertThat(user2.getName(), is(user.getName()));
+		assertThat(user2.getPassword(), is(user.getPassword()));
+	}
+}
