6915228906ef55c28ebd46c255a40f6090ab2d84
Soyoun Kim <makeajourney@gmail.com>
2016-07-14 11:29:52 +0900

    3.6.2 queryForInt()

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -7,9 +7,11 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
 
 import springbook.user.domain.User;
 
@@ -81,39 +83,17 @@ public class UserDao {
 	}
 	
 	public int getCount() throws SQLException {
-		Connection c = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
+		return this.jdbcTemplate.queryForInt("select count(*) from users");
 		
-		try {
-			c = dataSource.getConnection();
-		
-			ps = c.prepareStatement("select count(*) from users");
-		
-			rs = ps.executeQuery();
-			rs.next();
-			return rs.getInt(1);
-		} catch (SQLException e) {
-			throw e;
-		} finally {
-			if (rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e) {
-				}
-			}
-			if (ps != null) {
-				try {
-					ps.close();					
-				} catch (SQLException e) {					
-				}
-			}
-			if (c != null) {
-				try {					
-					c.close();
-				} catch (SQLException e) {
-				}
-			}
-		}
+//		return this.jdbcTemplate.query(new PreparedStatementCreator() {
+//			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+//				return con.prepareStatement("select count(*) from users");
+//			}
+//		}, new ResultSetExtractor<Integer>() {
+//			public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
+//				rs.next();
+//				return rs.getInt(1);
+//			}
+//		});
 	}
 }
