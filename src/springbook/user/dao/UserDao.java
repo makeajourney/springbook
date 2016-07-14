051afb5f47858fcd82a5d2649638ad38467889be
Soyoun Kim <makeajourney@gmail.com>
2016-07-14 13:14:40 +0900

    3.6.3 queryForObject()

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -1,17 +1,12 @@
 package springbook.user.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 
 import springbook.user.domain.User;
 
@@ -32,50 +27,17 @@ public class UserDao {
 	}
 	
 	public User get(String id) throws SQLException {
-		Connection c = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		
-		try {			
-			c = dataSource.getConnection();
-			ps = c.prepareStatement("select * from users where id = ?");
-			ps.setString(1, id);
-			
-			rs = ps.executeQuery();
-			
-			User user = null;
-			if (rs.next()) {
-				user = new User();			
-				user.setId(rs.getString("id"));
-				user.setName(rs.getString("name"));
-				user.setPassword(rs.getString("password"));
-			}
-			
-			if (user == null) throw new EmptyResultDataAccessException(1);
-			
-			return user;
-		} catch (SQLException e) {
-			throw e;
-		} finally {
-			if (rs != null) {
-				try {
-					rs.close();					
-				} catch (SQLException e) {
+		return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[] {id},
+			new RowMapper<User>() {
+				public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+					User user = new User();			
+					user.setId(rs.getString("id"));
+					user.setName(rs.getString("name"));
+					user.setPassword(rs.getString("password"));
+					return user;					
 				}
 			}
-			if (ps != null) {
-				try {
-					ps.close();					
-				} catch (SQLException e) {
-				}
-			}
-			if (c != null) {
-				try {					
-					c.close();
-				} catch (SQLException e) {
-				}
-			}
-		}
+		);
 	}
 	
 	public void deleteAll() throws SQLException {
@@ -84,16 +46,5 @@ public class UserDao {
 	
 	public int getCount() throws SQLException {
 		return this.jdbcTemplate.queryForInt("select count(*) from users");
-		
-//		return this.jdbcTemplate.query(new PreparedStatementCreator() {
-//			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-//				return con.prepareStatement("select count(*) from users");
-//			}
-//		}, new ResultSetExtractor<Integer>() {
-//			public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
-//				rs.next();
-//				return rs.getInt(1);
-//			}
-//		});
 	}
 }
