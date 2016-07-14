e953285ddf007c78f49b70f567016f0c017774da
Soyoun Kim <makeajourney@gmail.com>
2016-07-14 14:00:23 +0900

    3.6.5 재사용 가능한 콜백의 분리

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -12,6 +12,16 @@ import org.springframework.jdbc.core.RowMapper;
 import springbook.user.domain.User;
 
 public class UserDao {
+	private RowMapper<User> userMapper = 
+			new RowMapper<User>() {
+			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+				User user = new User();
+				user.setId(rs.getString("id"));
+				user.setName(rs.getString("name"));
+				user.setPassword(rs.getString("password"));
+				return user;
+			}
+		};
 	private JdbcTemplate jdbcTemplate;
 	
 	public void setDataSource(DataSource dataSource) {
@@ -25,17 +35,7 @@ public class UserDao {
 	}
 	
 	public User get(String id) throws SQLException {
-		return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[] {id},
-			new RowMapper<User>() {
-				public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-					User user = new User();			
-					user.setId(rs.getString("id"));
-					user.setName(rs.getString("name"));
-					user.setPassword(rs.getString("password"));
-					return user;					
-				}
-			}
-		);
+		return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[] {id}, this.userMapper);
 	}
 	
 	public void deleteAll() throws SQLException {
@@ -47,15 +47,6 @@ public class UserDao {
 	}
 	
 	public List<User> getAll() {
-		return this.jdbcTemplate.query("select * from users order by id", 
-			new RowMapper<User> () {
-				public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-					User user = new User();
-					user.setId(rs.getString("id"));
-					user.setName(rs.getString("name"));
-					user.setPassword(rs.getString("password"));
-					return user;
-				}
-		});
+		return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
 	}
 }
